0164195130a341a14870052981932dd08d6e8f89
Ruslan <rus@gmx.com>
2021-04-20 23:21:52 +0100

    Add API strings

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import Movie from './components/Movie';
 
+const FEATURED_API =
+  'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=11dbf833cc3f6316214d045528716be0';
+const IMG_API = 'https://image.tmdb.org/t/p/w1280';
+const SEARCH_API =
+  'https://api.themoviedb.org/3/search/movie?api_key=11dbf833cc3f6316214d045528716be0&query="';
+
 function App() {
   const movies = ['1', '2', '3'];
   return (
