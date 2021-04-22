2115b7c20b00bf717ae35022bbae6b6eec776633
Ruslan <rus@gmx.com>
2021-04-22 23:55:01 +0100

    Add movie vote_verage

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -5,6 +5,7 @@ const Movie = ({ title, poster_path, overview, vote_average }) => (
     <img src={IMG_API + poster_path} alt={title} />
     <div className="movie-info">
       <h3>{title}</h3>
+      <span>{vote_average}</span>
     </div>
   </div>
 );
