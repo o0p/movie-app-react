30d56398c0baa2680ff61c333938b8df62fc402a
Ruslan <rus@gmx.com>
2021-04-22 23:44:16 +0100

    Move IMG_API to Movie.jsx

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,4 +1,6 @@
-const Movie = () => {
+const IMG_API = 'https://image.tmdb.org/t/p/w1280';
+
+const Movie = ({ title, poster_path, overview, vote_average }) => {
   return <div className="movie">Movie component</div>;
 };
 
