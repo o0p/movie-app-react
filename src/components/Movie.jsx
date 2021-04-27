b664ab96e89aa4c04cf6db46739dfe61cfc06647
Ruslan <rus@gmx.com>
2021-04-27 15:49:03 +0100

    Add default movie img

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -12,7 +12,14 @@ const setVoteClass = (vote) => {
 
 const Movie = ({ title, poster_path, overview, vote_average }) => (
   <div className="movie">
-    <img src={IMG_API + poster_path} alt={title} />
+    <img
+      src={
+        poster_path
+          ? IMG_API + poster_path
+          : 'https://images.unsplash.com/photo-1589569334232-fdc917c38226?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8bW92aWUlMjAlMjBwcm9qZWN0b3J8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60'
+      }
+      alt={title}
+    />
     <div className="movie-info">
       <h3>{title}</h3>
       <span className={`tag ${setVoteClass(vote_average)}`}>
