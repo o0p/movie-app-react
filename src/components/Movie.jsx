27ab509c8d10f4fa058b5210bf9cb6bcc79ea865
Ruslan <rus@gmx.com>
2021-04-27 15:39:29 +0100

    Colorcoded movie ratings css

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -15,7 +15,9 @@ const Movie = ({ title, poster_path, overview, vote_average }) => (
     <img src={IMG_API + poster_path} alt={title} />
     <div className="movie-info">
       <h3>{title}</h3>
-      <span className={`tag ${setVoteClass(vote_average)}`}></span>
+      <span className={`tag ${setVoteClass(vote_average)}`}>
+        {vote_average}
+      </span>
     </div>
 
     <div className="movie-over">
